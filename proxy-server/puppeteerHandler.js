75d9729ac049b7f54c87fc45694eda09c63bfe18
CWD273 <itscwd273@gmail.com>
2025-09-21 07:57:53 -0500

    Update puppeteerHandler.js

--- proxy-server/puppeteerHandler.js
+++ proxy-server/puppeteerHandler.js
@@ -2,15 +2,16 @@ const puppeteer = require('puppeteer');
 
 async function fetchFilteredContent(targetUrl) {
   const browser = await puppeteer.launch({
-    headless: 'new',
-    args: ['--disable-web-security', '--no-sandbox'],
+    headless: false, // Extensions require non-headless mode
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      `--disable-extensions-except=${__dirname}/extension`,
+      `--load-extension=${__dirname}/extension`,
+    ],
   });
 
   const page = await browser.newPage();
-
-  // Load uBlock Origin extension
-  await page.goto('chrome-extension://<uBO-extension-id>/dashboard.html'); // preload rules if needed
-
   await page.goto(targetUrl, { waitUntil: 'networkidle2' });
   const content = await page.content();
 
