fcf297a67773d41e4d2a25a16afe38dc8a0d1f67
CWD273 <itscwd273@gmail.com>
2025-09-21 08:01:37 -0500

    Update server.js

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const { fetchFilteredContent } = require('./puppeteerHandler');
-const app = express();
+const { getCached, setCached } = require('./utils/cache');
 
 app.get('/filter', async (req, res) => {
   const { url } = req.query;
   if (!url) return res.status(400).send('Missing URL');
+
+  const cached = getCached(url);
+  if (cached) return res.send(cached);
+
   try {
     const content = await fetchFilteredContent(url);
+    setCached(url, content);
     res.send(content);
   } catch (err) {
     console.error(err);
     res.status(500).send('Filtering failed');
   }
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Proxy server running');
 });
